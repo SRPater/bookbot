ac54081db74ee9cbdb3576a7b67a281245d57821
SRPater <srpater@gmail.com>
2025-07-25 14:58:54 +0200

    CH3: Report, L2: Arguments

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import (
     get_num_words,
     get_chars_dict,
@@ -6,7 +7,11 @@ from stats import (
 
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
+    
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
@@ -29,7 +34,7 @@ def print_report(book_path, num_words, chars_sorted_list):
         if not item["char"].isalpha():
             continue
         print(f"{item["char"]}: {item["num"]}")
-        
+
     print("============= END =============")
 
 
